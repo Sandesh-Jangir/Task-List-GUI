e0bc7738485182ac4475d3270f003fea5483ce63
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-08-30 16:24:25 +0530

    Connected to database and now able to add the task

--- backend.py
+++ backend.py
@@ -1,4 +1,14 @@
 import eel
+import pymongo
+
 eel.init('web')
+
+client = pymongo.MongoClient('mongodb://localhost:27017')
+database = client['TaskList']
+collection = database['Tasks']
+task = {
+    "Task" : "Sample Task"
+}
+collection.insert_one(task)
 
 eel.start("index.html")
